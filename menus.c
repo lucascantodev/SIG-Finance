6e2138b94635efea0a622a8919dd6e8d64c65d3d
Lucas Silva Fernandes Canto <lucascantodev@gmail.com>
2021-10-21 11:42:51 -0300

    Menus

--- menus.c
+++ menus.c
@@ -6,8 +6,10 @@
 int main(void) { 
     cls;
 	printf("\n");
-		printf("/////////////////////////////////////////////////////////////////////////////\n");
-		printf("/////////////////////////////////////////////////////////////////////////////\n");
+    welcome();
+    printf("\n");
+	printf("///////////////////////////////////////////////////////////////////////////////////\n");
+	printf("///////////////////////////////////////////////////////////////////////////////////\n");
 	printf("/// | /////////// | /////////// | /////////// | /////////// | /////////// | ///////\n");
 	printf("///           ||||||===========|=============|==============||||||             ///\n");
 	printf("///////       ||||||||||||||||||||||||||||||||||||||||||||||||||||          //////\n");
@@ -27,19 +29,18 @@ int main(void) {
 	printf("///                                                                            ///\n");
 	printf("///                  FIRST OF ALL, WHAT'S YOUR NAME?                           ///\n");
     printf("///                                                                            ///\n");
-    printf("///                  PS.: LETTERS ONLY                                         ///\n");  
+    printf("///                        PS.: LETTERS ONLY                                   ///\n");  
     printf("///                                                                            ///\n");
 	printf("///                                                                            ///\n");
 	printf("///                                                                            ///\n");
     printf("///                                                                            ///\n");
 	printf("/// | /////////// | /////////// | /////////// | /////////// | /////////// | ////////\n");
-    	printf("///////////////////////////////////////////////////////////////////////////////\n");
-		printf("///////////////////////////////////////////////////////////////////////////////\n");
-	welcome();
+    printf("////////////////////////////////////////////////////////////////////////////////////\n");
+	printf("////////////////////////////////////////////////////////////////////////////////////\n");
 }
 
 void welcome(){
-	printf("__          __       _                                 _ \n\\ \\        / /      | |                               | |\n \\ \\  /\\  / /   ___ | |  ___   ___   _ __ ___    ___  | |\n  \\ \\/  \\/ /   / _ \\| | / __| / _ \\ | '_ ` _ \\  / _ \\ | |\n   \\  /\\  /   |  __/| || (__ | (_) || | | | | ||  __/ |_|\n    \\/  \\/     \\___||_| \\___| \\___/ |_| |_| |_| \\___| (_)\n");
+	printf("          __          __       _                                 _ \n          \\ \\        / /      | |                               | |\n           \\ \\  /\\  / /   ___ | |  ___   ___   _ __ ___    ___  | |\n            \\ \\/  \\/ /   / _ \\| | / __| / _ \\ | '_ ` _ \\  / _ \\ | |\n             \\  /\\  /   |  __/| || (__ | (_) || | | | | ||  __/ |_|\n              \\/  \\/     \\___||_| \\___| \\___/ |_| |_| |_| \\___| (_)\n");
 }
 
 void team(){
@@ -61,21 +62,21 @@ void about(){
 	printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-    printf("///               Departamento de Computação e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///           Projeto Sistema de Controle de Finanças Domésticas            ///\n");
-    printf("///                                   2021                                  ///\n");
+    printf("///             Federal University of Rio Grande do Norte                   ///\n");
+    printf("///                 Center for Higher Education of the Seridó               ///\n");
+    printf("///               Department of Computation and Technology                  ///\n");
+    printf("///                  Discipline DCT1106 -- Programming                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                                                                         ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///     = = = = = Sistema de Controle de Finanças Domésticas = = = = =      ///\n");
+    printf("//  = = = = = SHousehold Finance Control System Project - 2021  = = = = =   ///\n");
     printf("///                                                                         ///\n");
-    printf("///    Programa desenvolvido para disciplina DCT1106 -                      ///\n");
-    printf("///    O presente projeto tem como objetivo desenvolver um programa que     ///\n");
-    printf("///    realize o acompanhamento do fluxo financeiro em uma residência,      ///\n");
-    printf("///    evitando o descontrole de gastos.                                    ///\n");
+    printf("///    Application developed for the discipline DCT1106 - Programming       ///\n");
+    printf("///    The objective of this project is to develop an application that      ///\n");
+    printf("///    monitors the financial flow in a home and avoids                     ///\n");
+    printf("///    the lack of control of expenses.                                     ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
