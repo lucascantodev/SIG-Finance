6936e531328641fc7ea9f2923c45e6d7e35ed438
Jonathan Maia <jtauan10@gmail.com>
2021-10-20 22:08:05 -0300

    menusV1

---
+++ menus.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define cls system("clear||cls");
+
+int main(void) { 
+    cls;
+	printf("\n");
+		printf("/////////////////////////////////////////////////////////////////////////////\n");
+		printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("/// | /////////// | /////////// | /////////// | /////////// | /////////// | ///////\n");
+	printf("///           ||||||===========|=============|==============||||||             ///\n");
+	printf("///////       ||||||||||||||||||||||||||||||||||||||||||||||||||||          //////\n");
+	printf("///          |= = = = = = = = = = = = = = = = = = = = = = = = = =|             ///\n");
+	printf("///         |= = = =           SIG-FINANCE                 = = = =|            ///\n");
+	printf("///        |= = = =        Take control of your             = = = =|           ///\n");
+	printf("///        |= = = =       household expenses today!         = = = =|           ///\n");
+	printf("///           |= = = = = = = = = = = = = = = = = = = = = = = = =|              ///\n");
+	printf("///          |===================================================|             ///\n");
+	printf("///               Developed by Lucas Canto and Jonathan Tauan                  ///\n");
+  	printf("///                                                                            ///\n");
+	printf("//////////////////////////////////////////////////////////////////////////     ///\n");
+	printf("///                                                                            ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =                  ///\n");
+	printf("///         = = = = = = = = =  Start Menu  = = = = = = = = = =                 ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =                  ///\n");
+	printf("///                                                                            ///\n");
+	printf("///                  FIRST OF ALL, WHAT'S YOUR NAME?                           ///\n");
+    printf("///                                                                            ///\n");
+    printf("///                  PS.: LETTERS ONLY                                         ///\n");  
+    printf("///                                                                            ///\n");
+	printf("///                                                                            ///\n");
+	printf("///                                                                            ///\n");
+    printf("///                                                                            ///\n");
+	printf("/// | /////////// | /////////// | /////////// | /////////// | /////////// | ////////\n");
+    	printf("///////////////////////////////////////////////////////////////////////////////\n");
+		printf("///////////////////////////////////////////////////////////////////////////////\n");
+	welcome();
+}
+
+void welcome(){
+	printf("__          __       _                                 _ \n\\ \\        / /      | |                               | |\n \\ \\  /\\  / /   ___ | |  ___   ___   _ __ ___    ___  | |\n  \\ \\/  \\/ /   / _ \\| | / __| / _ \\ | '_ ` _ \\  / _ \\ | |\n   \\  /\\  /   |  __/| || (__ | (_) || | | | | ||  __/ |_|\n    \\/  \\/     \\___||_| \\___| \\___/ |_| |_| |_| \\___| (_)\n");
+}
+
+void team(){
+	printf("\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                              Developed by:                              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///             Lucas Canto           &           Jonathan Maia             ///\n");
+    printf("///             @lucascantodev                  @jonathan-tauan             ///\n");
+	printf("///                                                                         ///\n");
+    printf("///        Repository: https://github.com/lucascantodev/SIG-Finance         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void about(){
+	printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///           Projeto Sistema de Controle de Finanças Domésticas            ///\n");
+    printf("///                                   2021                                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///     = = = = = Sistema de Controle de Finanças Domésticas = = = = =      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///    Programa desenvolvido para disciplina DCT1106 -                      ///\n");
+    printf("///    O presente projeto tem como objetivo desenvolver um programa que     ///\n");
+    printf("///    realize o acompanhamento do fluxo financeiro em uma residência,      ///\n");
+    printf("///    evitando o descontrole de gastos.                                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+
+
+
