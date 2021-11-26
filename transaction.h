86e35699a160c5b1a8dea928aedf30160e06d06a
Lucas Silva Fernandes Canto <lucascantodev@gmail.com>
2021-11-26 16:46:20 -0300

    Separating Transaction  Module

---
+++ transaction.h
@@ -0,0 +1,19 @@
+// printf("///////////////////////////////////////////////////////////////////////////////\n\n");
+// printf("///                                                                         ///\n");
+// printf("///                              Developed by:                              ///\n");
+// printf("///                                                                         ///\n");
+// printf("\n///////////////////////////////////////////////////////////////////////////////\n");
+// printf("///             Lucas Canto           &           Jonathan Maia             ///\n");
+// printf("///             @lucascantodev                  @jonathan-tauan             ///\n");
+// printf("///                                                                         ///\n");
+// printf("///        Repository: https://github.com/lucascantodev/SIG-Finance         ///\n");
+// printf("///                                                                         ///\n");
+// printf("///////////////////////////////////////////////////////////////////////////////\n\n");
+
+//CRUD: Transactions
+void transactionMenu(void);
+void createTransaction(void);
+void transactionList(void);
+void detailTransaction(void);
+void updateTransaction(void);
+void deleteTransaction(void);
